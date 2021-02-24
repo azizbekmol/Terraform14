7cf5c090831ef6c232dbe74bd208107de1b24e35
azizbekmol <azizmdov@gmail.com>
2021-02-24 22:33:16 +0000

    added ec2

--- Class2/ec2.tf
+++ Class2/ec2.tf
@@ -1,6 +1,13 @@
 resource "aws_instance" "web" {
-  ami           = "ami-02354e95b39ca8dec"
-  instance_type = "t3.micro"
-  key_name      = aws_key_pair.deployer2.key_name
-  count         = 5
+  ami                         = "ami-02354e95b39ca8dec"
+  instance_type               = "t3.micro"
+  key_name                    = aws_key_pair.deployer2.key_name
+  count                       = 5
+  associate_public_ip_address = true
+  availability_zone           = "us-east-1a"
+  user_data                   = file("userdata.sh")
+  
+  tags = {
+    "Name" = "Aziz"
+  }
 }
