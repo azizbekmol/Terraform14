0f6a8d44d5d569dca6fb3b23c232de777a5f3d88
azizbekmol <azizmdov@gmail.com>
2021-03-03 21:15:00 +0000

    added

--- Class7/variable.tf
+++ Class7/variable.tf
@@ -1,8 +1,35 @@
 variable "region" {}
-variable "subnet_ids"{
-    type = list(any)
+variable "subnet_ids" {
+  type = list(any)
 }
 
 variable "db_access" {
-  type        = list(any)
+  type = list(any)
+}
+variable "identifier" {
+
+}
+variable "allocated_storage" {
+
+}
+variable "password" {
+    
+}
+variable "storage_type" {
+
+}
+variable "engine" {
+
+}
+variable "engine_version" {
+
+}
+variable "instance_class" {
+
+}
+variable "username" {
+
+}
+variable "publicly_accessible" {
+
 }
